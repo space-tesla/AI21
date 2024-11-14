632494434b7dede7b00ad3840673cc77254ee4a4
space-tesla <aspyrontech@gmail.com>
2024-11-14 23:39:55 +0530

    files added sucessfully !!!

--- A.py
+++ A.py
@@ -1,21 +1,13 @@
 # Q.1 Write a Python program to remove punctuations from the given string.
 
 import string
-
-# Function to remove punctuations from the given string
 def remove_punctuations(input_string):
-    # Create a translation table that maps punctuation characters to None
     translator = str.maketrans('', '', string.punctuation)
-    # Use translate to remove punctuation
     return input_string.translate(translator)
-
-# Input string from the user
 input_string = input("Enter a string: ")
-
-# Remove punctuations and print the result
 print("String without punctuation:", remove_punctuations(input_string))
 
 
-Output:
-Enter a string: Hello, world! How's it going?
-String without punctuation: Hello world Hows it going
+#Output:
+#Enter a string: Hello, world! How's it going?
+#String without punctuation: Hello world Hows it going
--- B.py
+++ B.py
@@ -1,81 +1,57 @@
 # Q.2 Write a Python program to implement the Heuristic Search algorithm.
 
-# Heuristic Search Algorithm Implementation (Greedy Best-First Search)
-
-# Define a Node class to represent each state in the search
-class Node:
-    def _init_(self, name, heuristic):
-        self.name = name        # Name of the node
-        self.heuristic = heuristic  # Heuristic value (estimated cost to goal)
-        self.parent = None      # Parent node to trace the path
-        self.children = []      # List of children nodes
-
-# Function to add children nodes to a parent node
-def add_child(node, child_node):
-    node.children.append(child_node)
-    child_node.parent = node
-
-# Heuristic Search Function (Greedy Best-First Search)
-def heuristic_search(start_node, goal_node):
-    open_list = [start_node]  # List of nodes to be evaluated
-    closed_list = []          # List of nodes that have already been evaluated
-    
+def heuristic_search(graph, heuristic, start, goal):
+    open_list = [(start, heuristic[start])] 
+    parents = {start: None}  
     while open_list:
-        # Sort open list based on heuristic value (greedy approach)
-        open_list.sort(key=lambda x: x.heuristic)
-        current_node = open_list.pop(0)  # Select the node with the lowest heuristic value
-        
-        print(f"Visiting node: {current_node.name}, Heuristic: {current_node.heuristic}")
-        
-        # If we have reached the goal, trace back the path
-        if current_node.name == goal_node.name:
+        open_list.sort(key=lambda x: x[1])
+        current, _ = open_list.pop(0)  
+
+        print(f"Visiting node: {current}, Heuristic: {heuristic[current]}")
+
+        if current == goal:
             path = []
-            while current_node:
-                path.append(current_node.name)
-                current_node = current_node.parent
-            path.reverse()
-            return path
-        
-        closed_list.append(current_node)  # Move the node to closed list
-        
-        # Add children of the current node to the open list
-        for child in current_node.children:
-            if child not in open_list and child not in closed_list:
-                open_list.append(child)
-    
-    return None  # No path found
+            while current:
+                path.append(current)
+                current = parents[current]
+            return path[::-1]  
 
-# Example problem: Search from 'A' to 'F'
-start = Node('A', 6)
-goal = Node('F', 0)
+        for neighbor in graph.get(current, []):
+            if neighbor not in parents:  
+                parents[neighbor] = current
+                open_list.append((neighbor, heuristic[neighbor]))
 
-B = Node('B', 4)
-C = Node('C', 3)
-D = Node('D', 1)
-E = Node('E', 2)
+    return None  
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D'],
+    'C': ['E'],
+    'D': ['F'],
+    'E': ['F']
+}
 
-# Adding children to each node
-add_child(start, B)
-add_child(start, C)
-add_child(B, D)
-add_child(C, E)
-add_child(D, goal)
-add_child(E, goal)
+heuristic = {
+    'A': 6,
+    'B': 4,
+    'C': 3,
+    'D': 1,
+    'E': 2,
+    'F': 0
+}
 
-# Perform Heuristic Search
-path = heuristic_search(start, goal)
+path = heuristic_search(graph, heuristic, 'A', 'F')
 
-# Output the path if found
 if path:
     print("Path to goal:", " -> ".join(path))
 else:
     print("No path found")
 
 
-Output:
+
+"""Output:
 Visiting node: A, Heuristic: 6
 Visiting node: C, Heuristic: 3
 Visiting node: E, Heuristic: 2
 Visiting node: D, Heuristic: 1
 Visiting node: F, Heuristic: 0
-Path to goal: A -> C -> E -> F
+Path to goal: A -> C -> E -> F"""
